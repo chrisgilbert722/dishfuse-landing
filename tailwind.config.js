1722a3e1d111285dc1e6a39944f42d027fd23d95
Chris Gilbert <chrisgilbert@MacBookPro.lan>
2025-10-30 06:59:32 -0400

    Add custom navy and gold colors for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        navy: {
+          900: "#0A1120",
+          800: "#0B1222",
+          700: "#102042",
+          600: "#1F2D50",
+          500: "#24335D"
+        },
+        gold: {
+          500: "#F4C762",
+          600: "#EEB94A"
+        }
+      }
+    }
   },
   plugins: []
 };
