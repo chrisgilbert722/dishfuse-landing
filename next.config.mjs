7c783ea48339d0c026ae4aba84163f86ab3d6fe8
Chris Gilbert <chrisgilbert@MacBookPro.lan>
2025-10-30 16:53:59 -0400

    Ensure ignoreBuildErrors true in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // completely ignore ESLint during builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // completely ignore TypeScript errors during builds
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  // optional Framer Motion optimization
   experimental: {
-    optimizePackageImports: ["framer-motion"]
-  }
+    optimizePackageImports: ["framer-motion"],
+  },
 };
 
 export default nextConfig;
